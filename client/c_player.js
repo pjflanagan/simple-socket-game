4c670deda3cda917ba29beeb6d6042952a42a500
Peter James Flanagan <pjflanagan1@gmail.com>
2019-09-17 00:38:31 -0400

    hud

--- client/c_player.js
+++ client/c_player.js
@@ -4,6 +4,7 @@ class Player {
 	constructor(app) {
 		this.app = app;
 
+		this.timeout = 0;
 		this.rounds = LASER_PROPS.ROUNDS;
 		this.mouse = { x: 0, y: 0 };
 		this.values = {
@@ -13,7 +14,7 @@ class Player {
 
 		window.addEventListener("mousemove", (e) => { this.mouse.x = e.clientX; this.mouse.y = e.clientY; });
 		window.addEventListener("mousedown", (e) => { this.handleClick(); })
-		// document.addEventListener("visibilitychange", (e) => { this.defaultKeys(); });
+		document.addEventListener("visibilitychange", (e) => { this.defaultKeys(); });
 
 	}
 
@@ -35,10 +36,11 @@ class Player {
 			newKeys.r !== this.values.keys.r;
 	}
 
-	// defaultKeys() {
-	// 	this.values.keys = { u: false, d: false, r: false, l: false };
-	// 	this.app.sendKeyChange();
-	// }
+	defaultKeys() {
+		console.log('DEAFAULT KEYS')
+		this.values.keys = { u: false, d: false, r: false, l: false };
+		this.app.sendKeyChange();
+	}
 
 	get angle() {
 		return this.values.angle;
@@ -59,6 +61,7 @@ class Player {
 		if (this.rounds === 0)
 			return;
 		this.rounds -= 1;
+		this.app.updateHUD();
 		this.reload();
 
 		let x = this.app.self.body.x + this.app.self.body.halfWidth;
@@ -68,7 +71,11 @@ class Player {
 
 	reload() {
 		if (this.rounds === 0) {
-			setTimeout(() => { this.rounds = LASER_PROPS.ROUNDS; }, LASER_PROPS.RELOAD_INTERVAL);
+			// this.clearTimeout(this.timeout)
+			this.timeout = setTimeout(() => {
+				this.rounds = LASER_PROPS.ROUNDS;
+				this.app.updateHUD();
+			}, LASER_PROPS.RELOAD_INTERVAL);
 		}
 	}
 
